0479e2f0831a230b34180570465b722e352def6a
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2011-06-23 11:16:47 +0300

    flickr allow save as script

---
+++ flickrAllowSaveAs.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           flickrAllowSaveAs
+// @description    for logged in flickr users - removes spaceball div
+// @copyright      2010, Hasan Tayyar BEŞİK (http://hasantayyar.com)
+// @license        LGPL 
+// @include        http://www.flickr.com/*
+// ==/UserScript==
+
+function removeDivL1(){
+ pl1 = document.getElementById("allsizes-photo");
+ sl1 = pl1.children[0] 
+ pl1.removeChild(sl1);
+}
+removeDivL1();
