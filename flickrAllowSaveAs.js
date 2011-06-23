b7705f82324c440c1faab7fe06418a45762fea24
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2011-06-23 11:23:46 +0300

    id var

--- flickrAllowSaveAs.js
+++ flickrAllowSaveAs.js
@@ -6,9 +6,9 @@
 // @include        http://www.flickr.com/*
 // ==/UserScript==
 
-function removeDivL1(){
- pl1 = document.getElementById("allsizes-photo");
+function removeDivL1(id){
+ pl1 = document.getElementById(id);
  sl1 = pl1.children[0] 
  pl1.removeChild(sl1);
 }
-removeDivL1();
+removeDivL1('allsizes-photo');
